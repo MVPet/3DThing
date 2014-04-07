1f4916ca05c94d5e07a8ae0aca49259fbae62ee4
MVPet <petrarcamarcox@my.uri.edu>
2014-04-06 22:32:30 -0400

    April 6, 2014
    Removed unused files...
    ...which fixed the Unity Editor crashing
    Added an NPC

---
+++ Assets/Scripts/CharacterController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterController : MonoBehaviour {
+
+    public Animator anim;
+    public float forwardMove = 0;
+    public float horizontalMove = 0;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        horizontalMove = Input.GetAxis("Horizontal");
+        anim.SetFloat("Turn", horizontalMove);
+
+        forwardMove = Input.GetAxis("Vertical");
+        anim.SetFloat("Forward", forwardMove);
+    }
+}
